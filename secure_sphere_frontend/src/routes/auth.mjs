6aaa3698712a70592117ad8c1b4c22b918ab17db
GubbinLubbin <silwalankit27@gmail.com>
2025-12-01 05:54:07 +0530

    added the auth logic in the schemas using the concept of next and methods

---
+++ secure_sphere_frontend/src/routes/auth.mjs
@@ -0,0 +1,2 @@
+import auth from "../schemas/authschemas.mjs";
+import { Router } from "express";
--- secure_sphere_frontend/src/schemas/authschemas.mjs
+++ secure_sphere_frontend/src/schemas/authschemas.mjs
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt"
 const authSchemas = mongoose.Schema(
   {
     username: {
@@ -8,6 +9,7 @@ const authSchemas = mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique:true
     },
     password: {
       type: String,
@@ -16,11 +18,25 @@ const authSchemas = mongoose.Schema(
     role: {
       type: String,
       enum: ["admin", "user"],
-      default: user,
+      default:"user",
     },
   },
   {
     timestamps: true,
   }
 );
-export default authSchemas
+authSchemas.pre("save",async function (next){
+  if(!this.isModified("password")){
+    return next();
+  }
+  const saltRounds=10
+  this.password=await bcrypt.hash(this.password,saltRounds)
+  next();
+})
+authSchemas.method.comparePassword=async function (enteredPassword){
+  return bcrypt.compare(enteredPassword,this.password)
+}
+
+
+const auth=mongoose.model("auth",authSchemas)
+export default auth
