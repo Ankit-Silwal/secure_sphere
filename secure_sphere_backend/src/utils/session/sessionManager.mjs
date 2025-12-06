d13e72b9b79b241f49318861592c1e45b5a94f70
GubbinLubbin <silwalankit27@gmail.com>
2025-12-06 19:02:06 +0530

    Added the delete in the session

--- secure_sphere_backend/src/utils/session/sessionManager.mjs
+++ secure_sphere_backend/src/utils/session/sessionManager.mjs
@@ -1,6 +1,5 @@
 import {randomBytes} from "crypto"
 import { redisClient } from "../../configs/redis.mjs"
-import { devNull } from "os"
 
 function generateSessionId(){
   return randomBytes(32).toString('hex')
@@ -42,4 +41,24 @@ export async function getSession(sessionId){
   }
 
   return JSON.parse(sessionData)
+}
+
+export async function deleteSession(sessonId){
+  if(!sessionId){
+    return false
+  }
+
+  const session=await getSession(sessionId);
+  if(session){
+    const userSessionKey=`user:session:${session.userId}`
+    await redisClient.sRem(userSessionKey,sessionId)
+  }
+  const sessionKey=`session:${sessionId}`
+  const result=await redisClient.del(sessionKey)
+
+  if(result>0){
+    console.log(`Session deleted :${sessonId}`)
+    return true
+  }
+  return false
 }
